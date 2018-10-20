09c9290e9275ff3d53fc8b7867ef272cedfba708
Octave Raimbault <me@dispix.net>
2018-10-20 23:55:11 +0200

    chore(*): switch to node-fetch and update dependencies

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
-const fs = require('fs')
-const path = require('path')
-const OpenSubtitlesApi = require('opensubtitles-api')
-const { XMLHttpRequest } = require('xmlhttprequest')
+const fs = require("fs");
+const path = require("path");
+const OpenSubtitlesApi = require("opensubtitles-api");
+const fetch = require("node-fetch");
 
 // Available languages
-const languages = new Map([
-  ['fr', 'fre'],
-  ['en', 'eng']
-])
+const languages = new Map([["fr", "fre"], ["en", "eng"]]);
 
 // Handling arguments
-const source = process.argv[2].split('/')
-const lang = languages.has(process.argv[3]) ? process.argv[3] : 'en'
-const dest = process.argv[4] || source.slice(0, source.length - 1).join('/')
+const source = process.argv[2].split("/");
+const lang = languages.has(process.argv[3]) ? process.argv[3] : "en";
+const dest = process.argv[4] || source.slice(0, source.length - 1).join("/");
 
 // Building subtitle name
-let filename = source[source.length - 1]
-filename = filename.slice(0, filename.lastIndexOf('.')) + '.srt'
+let filename = source[source.length - 1];
+filename = filename.slice(0, filename.lastIndexOf(".")) + ".srt";
 
 // Creating new api
 const api = new OpenSubtitlesApi({
-  useragent: 'OSTestUserAgentTemp'
-})
+  useragent: "PlayMe v1"
+});
 
 // Search for the subtitle with the api, using the filepath as research parameter
-api.search({
-  sublanguageid: languages.get(lang),
-  extensions: ['srt'],
-  limit: '1',
-  path: source.join('/')
-})
-  .then((subs) => {
+
+(async function() {
+  try {
+    const subs = await api.search({
+      sublanguageid: languages.get(lang),
+      extensions: ["srt"],
+      limit: "1",
+      path: source.join("/")
+    });
+
     if (subs[lang] === undefined) {
-      console.warn('No subtitle found for this file.')
-      return
+      console.warn("No subtitle found for this file.");
+      return;
     }
 
-    // Initiating new XHR call if an url is available
-    const xhr = new XMLHttpRequest()
+    const response = await fetch(subs[lang][0].url);
 
-    // Save subtitle after the XHR call has ended
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        fs.writeFile(path.join(dest, filename), xhr.responseText, (err) => {
-          if (err) throw err
+    if (response.ok) {
+      const file = await response.text();
 
-          console.log('Subtitles downloaded succesfuly.')
-          return
-        })
-      }
+      fs.writeFile(path.join(dest, filename), file, err => {
+        if (err) throw err;
+
+        console.log("Subtitles downloaded succesfuly.");
+        return;
+      });
     }
-
-    xhr.open('GET', subs[lang][0].url)
-    xhr.send()
-  })
-  .catch((err) => console.error(err))
+  } catch (e) {
+    console.error(e);
+  }
+})();
